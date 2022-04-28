9bf89a19710a7ea1b5920398340f60eadb61c64f
Ujjwal R <ujjwal.raj.5798@gmail.com>
2022-04-28 21:19:35 +0530

    commit before final test

--- auto1.sh
+++ auto1.sh
@@ -2,14 +2,20 @@
 
 #This script helps in creating and pushing files in a github repository
 
+# if [[ "${UID}" -ne 0 ]]
+# then
+#     echo 'Please run with sudo or root.' >&2
+#     exit 1
+# fi
+
 CURRENTDIR=${pwd}
 
-instruct() {
-    echo "Usage: ${0} [-vs] [-l LENGTH]" >&2
-    echo 'Generate a random password.' >&2
-    echo '  -l LENGTH  Specify the password length.' >&2
-    echo '  -s         Append a special character to the password.' >&2
-    echo '  -v         Increase verbosity.' >&2
+help() {
+    echo "reset username password"
+}
+
+pushinstruct() {
+    echo "Usage: ${0} [repositoryName]" >&2
     exit 1
 }
 
@@ -18,7 +24,7 @@ firstinstruct() {
     echo 'Setup username and password for repeatetive usage.' >&2
     echo '  username:     Specify github username.' >&2
     echo '  password:     Specify github password.' >&2
-    
+    exit 1
 }
 
 
@@ -26,8 +32,18 @@ firstinstruct() {
 #Checking if username,password file exists or not
 if [[ ! -f "usrpswd" ]]
 then
+    echo "" > "usrpswd"
     echo "First Time User? Lets jump in."    
     firstinstruct
+fi
+
+if [[ "${#}" -lt 1 ]]
+then
+    firstinstruct
+fi
+
+if [[ "${#}" -eq 2 ]]
+then
     USRN=${1}
     PASS=${2}
     echo "${USRN}" > "usrpswd"
@@ -39,10 +55,47 @@ then
     else
         echo "Username and password successfully stored for further usage."
         echo "Congratulations! You are good to go."
+        pushinstruct
     fi
+elif [[ "${#}" -eq 1 ]]
+then
+    echo "Give name to your remote repository:"
+    read REPONAME
+    echo "Repo description: "
+    read DESC
+    echo "Project Path: "
+    read PROPATH
+
+    cd "${PROPATH}"
+
+    git init
+
+    echo "Create ReadMe?(y/n)"
+    read TEMP
+    if [[ TEMP == "y" ]]
+    then
+        touch README.MD
+        git add README.MD
+        git commit -m 'first commit, repo created.'
+    fi
+
+    USERNAME=$(head -n 1 "usrpswd")
+
+    curl -H "Authorization: token $PASS" https://api.github.com/user/repos -d "{"name":"${REPONAME}"}"
+
+    git remote add origin https://github.com/${USERNAME}/${REPONAME}.git
+    git push --set-upstream origin master
+    cd "$PROJECT_PATH"
+
+    echo "Done. Go to https://github.com/$USERNAME/$REPO_NAME to see." 
+    echo " *** You're now in your project root. ***"
+else
+    firstinstruct
 fi
 
-echo "${USRN}"
-echo "${PASS}"
+
+
+# echo "${USRN}"
+# echo "${PASS}"
 
 exit 0
